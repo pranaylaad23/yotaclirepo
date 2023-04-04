63cc1ab0e147c9e3c0bb0417cad7c22e0df51989
Ni3Chougale <113974022+Ni3Chougale@users.noreply.github.com>
2023-04-04 15:03:25 +0530

    modified

--- yotacli/src/App.js
+++ yotacli/src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import './App.css';
 import ListTechnology from './component/technology/ListTechnology';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <ListTechnology/>
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route path='/listTechnology' element={<ListTechnology/>}/>
+      </Routes>
+    </React.Fragment>
   );
 }
 
--- yotacli/src/component/technology/HeaderItem.js
+++ yotacli/src/component/technology/HeaderItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import classes from '../../ui/card/Card.module.css';
 import Button from '../../ui/button/Button';
-import './HeaderItemModule.css';
+import classes from './HeaderItem.module.css';
 import InputField from '../../ui/inputField/InputField';
 
 const HeaderItem = () => {
@@ -11,7 +10,7 @@ const HeaderItem = () => {
 
         <div className='col-xl-8 col-lg-7 col-md-6 col-sm-4'>
           <h5 className={classes.boxtitle}>List Of Technology
-            <button className='btn-filter'> <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
+            <button className={classes.btnfilter}> <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
               fill="currentColor" className='bi bi-filter' viewBox="0 0 16 16">
               <path
                 d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
@@ -26,7 +25,7 @@ const HeaderItem = () => {
               <div><i className="fa fa-filter" style={{ color: 'white'}}></i></div>
             </Button> */}
             <InputField>
-              <input className="inputField " type="search" placeholder="Search keyword..." aria-label="Search" />
+              <input className={classes.inputField} type="search" placeholder="Search keyword..." aria-label="Search" />
             </InputField>
             <Button><i className='fa fa-search ' style={{ color: 'white' }}></i></Button>
           </form>
