e7d8c24784003dadacb4f47718fd6f505e878c4b
Ni3Chougale <113974022+Ni3Chougale@users.noreply.github.com>
2023-04-04 19:10:30 +0530

    changes commited

--- yotacli/src/index.js
+++ yotacli/src/index.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 // import $ from 'jquery';
 // import Popper from 'popper.js';
 import React from 'react';
