7009b0e84fb8d7704adc3f52ae097bfca91984ac
Sharad Yuvraj Patil <sharad.patil@Yash.com>
2023-04-04 11:21:37 +0530

    created dashboard

---
+++ yotacli/src/components/dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Dashboard.module.css";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import TitleDashboard from "./TitleDashboard";
+
+function Dashboard() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <Sidebar />
+        <div className="col-xl-10 col-lg-10" style={{ padding: "0px 0px" }}>
+          <Navbar />
+          <TitleDashboard />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
