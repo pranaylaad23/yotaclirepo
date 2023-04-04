a9784fd3d3aa86621de5db63e875d828449f1472
anilshimpi <anil.shimpi4046@gmail.com>
2023-04-04 10:28:59 +0530

    created BatchList module

--- yotacli/src/App.js
+++ yotacli/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BatchList from './components/batch/BatchList';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment >
+      <Routes>
+        
+        <Route path="batchlist" element={<BatchList />} />
+      </Routes>
+      <BatchList/>
+    </React.Fragment>
   );
 }
 
