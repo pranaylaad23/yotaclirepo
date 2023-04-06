a108a22faa9e6f7ec4f8cd8cc1c3d50f97bec3a2
anilshimpi <106221933+anilshimpi@users.noreply.github.com>
2023-04-06 12:45:51 +0530

    made change in App.js file

--- yotacli/src/App.js
+++ yotacli/src/App.js
@@ -4,8 +4,6 @@ import "./App.css";
 import BatchList from "./components/batch/BatchList";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
-import ListQuestions from "./components/Question/ListQuestions";
-import ListTechnology from "./components/technology/ListTechnology";
 
 function App() {
   return <Dashboard />;
