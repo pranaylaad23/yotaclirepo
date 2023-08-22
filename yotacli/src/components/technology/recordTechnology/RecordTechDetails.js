100823d9124d6874fae5c30ea5ede6eb2ac2c5c0
DarshanS001 <97165858+DarshanS001@users.noreply.github.com>
2023-08-22 13:57:34 +0530

    Update RecordTechDetails.js

--- yotacli/src/components/technology/recordTechnology/RecordTechDetails.js
+++ yotacli/src/components/technology/recordTechnology/RecordTechDetails.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from "../../../components/technology/recordTechnology/RecordTechDetails.module.css";
 import { useParams } from 'react-router-dom';
+import { fetchTechnologyTestDetails } from "../../../redux/features/technology/CreateTechSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const RecordTechDetails = () => {
     const { name } = useParams();
     console.log("UseParam name in RecordTechDetails.js", name);
+    const dispatch = useDispatch();
+
+    const technologyTestDetails = useSelector((state) => state.technology.testDetailsArray);
+    console.log("original array in List Record technologyTestDetails",technologyTestDetails);
+
+  useEffect(() => {
+      dispatch(fetchTechnologyTestDetails(name));
+  }, []);
 
   return (
     <div>
@@ -23,10 +33,31 @@ const RecordTechDetails = () => {
           </tr>
         </thead>
         <tbody>
-          <td>A</td>
-          <td>A</td>
-          <td>A</td>
-          <td><i className="fa fa-edit"></i>&nbsp;{" "}</td>
+        {technologyTestDetails.length && technologyTestDetails[0].length > 0 ? technologyTestDetails[0].map((tech, key) => (
+              <tr key={key}>
+                <td>
+                  <b>{key}</b>
+                </td>
+                <td>{tech.testName}</td>
+                <td>{tech.testDescription}</td>
+                <td>
+                  <i className="fa fa-edit"></i>&nbsp;{" "}
+                  <i className="fa fa-trash-can"></i>{" "}
+                </td>
+                
+              </tr>
+            )):
+
+            <tr>
+          <td></td>
+          <td>
+            <h3>No Test Found</h3>
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+            }
+          
         </tbody>
       </table>
     </div>
@@ -34,5 +65,5 @@ const RecordTechDetails = () => {
     </div>
   )
 }
-
+        
 export default RecordTechDetails
