e50118687b0c3002c6da72d9494be35cda442459
DarshanS001 <97165858+DarshanS001@users.noreply.github.com>
2023-08-22 13:35:01 +0530

    Update dynamic Pagination for record technology page

--- yotacli/src/ui/pagination/Pagination.js
+++ yotacli/src/ui/pagination/Pagination.js
@@ -3,41 +3,66 @@ import classes from "./Pagination.module.css"
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-const Pagination = ({dataPerPage, setCurrentPage, currentPage}) => {
+const Pagination = ({dataPerPage, setCurrentPage, currentPage, toggleRecord}) => {
   // const [dataPerPage, setDataPerPage] = useState(5); //Pagination
   const technology = useSelector((state) => state.technology);
+  const technologyTestNumber = useSelector((state) => state.technology.testNumberArray);
 
+  // For Search Technology
   const totalData = technology.searchTech.length;
   console.log("total Data:",totalData);
 
+  // For Record Data Technology
+  const totalTestData = technologyTestNumber.length;
+  console.log("totalTestData",totalTestData);
+
+  // For Search Technology
   let totalSearchData;
   if(totalData !== 0){
     totalSearchData = technology.searchTech[0].length;
     console.log("total Search Data:",totalSearchData);
   }
   
-  
+  // For Record Data Technology
+  let totalTestPageData;
+  if(totalTestData!==0){
+    totalTestPageData = technologyTestNumber[0].length;
+    console.log("totalTestPageData:",totalTestPageData);
+  }
 
   console.log("dataPerPage:",dataPerPage);
   
-
   let pages = [];
 
+  // For Search Technology
   if(totalSearchData !== undefined){
-    console.log("IN total Search Data");
+    pages = []
+    console.log("IN total Search Data", pages);
     console.log("totalSearchData/dataPerPage:",totalSearchData/dataPerPage);
     for (let i = 1; i <= Math.ceil(totalSearchData/dataPerPage); i++){
       pages.push(i);
     }
   }else{
-    console.log("IN Normal Data");
+    pages = []
+    console.log("IN Normal Data", pages);
     console.log("totalData/dataPerPage:",totalData/dataPerPage);
     for (let i = 1; i <= Math.ceil(totalData/dataPerPage); i++){
       pages.push(i);
     }
   }
-  
   console.log("pages:",pages.length);
+
+// For Record Data Technology
+if (toggleRecord){
+  if(totalTestPageData > 0){
+    pages = []
+    console.log("IN total test Data per page", pages);
+    console.log("totalTestPageData/dataPerPage:",totalTestPageData/dataPerPage);
+    for (let i = 1; i <= Math.ceil(totalTestPageData/dataPerPage); i++){
+      pages.push(i);
+    }
+  }
+}
 
   const previousPage = () => {
     if (currentPage !== 1){
