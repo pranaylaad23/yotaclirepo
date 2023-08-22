692507ddaee753ac90e905145465a4c463478e64
DarshanS001 <97165858+DarshanS001@users.noreply.github.com>
2023-08-22 13:54:52 +0530

    Update ListRecord.js

--- yotacli/src/components/technology/recordTechnology/ListRecord.js
+++ yotacli/src/components/technology/recordTechnology/ListRecord.js
@@ -7,58 +7,62 @@ import { Link } from "react-router-dom";
 const ListRecord = ({currentPage, dataPerPage}) => {
   const dispatch = useDispatch();
 
-  const technology = useSelector((state) => state.technology.technologies);
-   useEffect(() => {
-    dispatch(fetchTechnology());
-  }, []);
-
-  // Pending from backend due to Wrong JSON format
-  // const technologyTestNumber = useSelector((state) => state.technology.testNumberArray);
-  // console.log("original array in List Record technologyTestNumber",technologyTestNumber);
-
-  // useEffect(() => {
-  //   dispatch(fetchTechnologyTestNumber());
+  // const technology = useSelector((state) => state.technology.technologies);
+  //  useEffect(() => {
+  //   dispatch(fetchTechnology());
   // }, []);
+
+  const technologyTestNumber = useSelector((state) => state.technology.testNumberArray);
+  console.log("original array in List Record technologyTestNumber",technologyTestNumber);
+
+  useEffect(() => {
+      dispatch(fetchTechnologyTestNumber());
+  }, []);
 
 
 
   //Pagination
   console.log("currentPage:", currentPage);
+  console.log("dataPerPage:", dataPerPage);
   const lastDataIndex =  currentPage * dataPerPage;
   const firstDataIndex =  lastDataIndex - dataPerPage;
+  console.log("lastDataIndex:", lastDataIndex);
+  console.log("firstDataIndex:", firstDataIndex);
 
-  const paginatedData =  technology.slice(firstDataIndex,lastDataIndex);
-  console.log("Data per page to show:", paginatedData);
+  let paginatedData;
+  if (technologyTestNumber.length !== 0){
+    if(technologyTestNumber[0].length > 0){
+      paginatedData =  technologyTestNumber[0].slice(firstDataIndex,lastDataIndex);
+      console.log("Data per page to show:", paginatedData);
 
-
-  return (
-    <div className={`table-responsive ${classes.table}`}>
-      <table className="table table-bordered table-hover">
-        <thead>
-          <tr>
-            <th>Sr.No.</th>
-            <th>Name Of Technology</th>
-            {/* <th>Description</th> */}
-            <th>Test Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          
-          {paginatedData.map((tech, key) => (
-          <tr key={key}>
-            <td>
-              <b>{key + firstDataIndex + 1}</b>
-            </td>
-            <td>{tech.name}</td>
-            {/* <td>{tech.shortDescription}</td> */}
-            <td><Link to={`/recordTechDetails/${tech.name}`}>2</Link></td>
-          </tr>
-        ))}
-
-        </tbody>
-      </table>
-    </div>
-  )
+      return (
+        <div className={`table-responsive ${classes.table}`}>
+          <table className="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>Sr.No.</th>
+                <th>Name Of Technology</th>
+                <th>Test Number</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+              {paginatedData.map((tech, key) => (
+              <tr key={key}>
+                <td>
+                  <b>{key + firstDataIndex + 1}</b>
+                </td>
+                <td>{tech.name}</td>
+                <td><Link to={`/recordTechDetails/${tech.name}`}>{tech.testCount}</Link></td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+  }
+  
 }
 
 export default ListRecord
