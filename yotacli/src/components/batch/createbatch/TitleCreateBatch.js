ecb58c0704f45d35087951d04486f08ac4acaf32
nidhi-sharma96 <141110995+nidhi-sharma96@users.noreply.github.com>
2023-10-26 18:06:20 +0530

    made Ui changes for create batch page

--- yotacli/src/components/batch/createbatch/TitleCreateBatch.js
+++ yotacli/src/components/batch/createbatch/TitleCreateBatch.js
@@ -8,7 +8,7 @@ const TitleCreateBatch = () => {
     {/* <!--dashbord--> */}
     <div className="container-fluid">
       <div className={`row ${classes.back}`}>
-        <div className="col" style={{textAlign:"center"}}>
+        <div className="col">
           <h5 className={classes.title}>Batch Management</h5>
         </div>
       </div>
