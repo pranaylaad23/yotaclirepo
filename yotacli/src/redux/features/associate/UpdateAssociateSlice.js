c7c517b4bbaef9dc7ec159957857aad4f2f8a112
Pravin Navarkar <pravin.navarkar@tenerity.com>
2023-11-03 11:04:12 +0530

    modified emailId and password fields in one row for add associate

--- yotacli/src/redux/features/associate/UpdateAssociateSlice.js
+++ yotacli/src/redux/features/associate/UpdateAssociateSlice.js
@@ -1,45 +1,70 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 /* Update Associate action */
-export const updateAssociate = createAsyncThunk("updateassociate", async (data, { rejectedWithValue }) => {
-    const response = await fetch('http://localhost:9090/yota/api/associates/', {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    })
+// export const updateAssociate = createAsyncThunk("updateassociate", async (data, { rejectedWithValue }) => {
+//     const response = await fetch('http://localhost:9090/yota/api/associates/', {
+//         method: "POST",
+//         headers: {
+//             Accept: "application/json",
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(data)
+//     })
 
-    try {
-        const result = await response.json();
-        console.log(result);
-        return result;
+//     try {
+//         const result = await response.json();
+//         console.log(result);
+//         return result;
+//     }
+//     catch(error){
+//         return rejectedWithValue(error);
+//     }
+// })
+
+export const UpdateAssociate = createAsyncThunk(
+    "UpdateAssociate",
+    async (data, { rejectWithValue }) => {
+      try {
+        debugger;
+        console.log("testt==" + data.shortDescription);
+        axios
+          .put(`http://localhost:9090/yota/api/associates/${data.id}`, data)
+          .then((res) => {
+            console.log(res.data);
+          });
+      } catch (error) {
+        return rejectWithValue(error);
+      }
     }
-    catch(error){
-        return rejectedWithValue(error);
-    }
-})
+  );
 
 export const associateUpdate = createSlice ({
     name : "associateUpdate",
     initialState : {
-        associate : [],
+        associates : [],
         loading : false,
         error : null,
     },
 
     extraReducers:{
-        [updateAssociate.pending] : (state) => {
+        [UpdateAssociate.pending] : (state) => {
             state.loading = true;
         },
 
-        [updateAssociate.fulfilled] : (state, action) => {
+        [UpdateAssociate.fulfilled]: (state, action) => {
             state.loading = false;
-            state.associate = action.payload;
-        },
+            const {
+              arg: { id },
+            } = action.meta;
+            if (id) {
+              state.associates = state.associates.map((item) =>
+                item._id === id ? action.payload : item
+              );
+            }
+          },
 
-        [updateAssociate.rejected] : (state, action) => {
+        [UpdateAssociate.rejected] : (state, action) => {
             state.loading = false;
             state.error = action.payload;
         },
