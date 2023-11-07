e016aa17020193d883ae496ec2f629ba51a950c1
Pragati Sanjay Paliwal <pragati.paliwal@yash.com>
2023-11-07 11:41:08 +0530

    added search bar in yota-ui-trainee-dashboard

--- yotacli/src/components/trainee/dashboard-layout/TraineeNavbar.js
+++ yotacli/src/components/trainee/dashboard-layout/TraineeNavbar.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Container from "react-bootstrap/Container";
-import { Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Button, Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import navcCass from "./TraineeNavbar.module.css";
 import { useDispatch } from "react-redux";
-
+import "./styles.css";
 
 const TraineeNavbar = (props) => {
   const onHamburgerClick = () => {
@@ -12,17 +12,18 @@ const TraineeNavbar = (props) => {
   };
 
   var barsIcon = <i className="fas fa-bars"></i>;
-  var crossIcon = <i className="fas fa-times-circle"></i>; 
+  var crossIcon = <i className="fas fa-times-circle"></i>;
 
   return (
     //className={`navbar-brand ${classes.yashimage}`}
     <div className={`${navcCass.headerContainer}`}>
       <Navbar expand="lg" className={`navbar navbar-dark ${navcCass.bgColor}`}>
-        <Container fluid >
+        <Container fluid>
           {/* <i class="fa-solid fa-bars" href="#"></i> */}
-          <i           
+          <i
             onClick={onHamburgerClick}
-            className={`bi bi-list ${navcCass.sidebarCollapse}`}>
+            className={`bi bi-list ${navcCass.sidebarCollapse}`}
+          >
             <span className={props.isNotActive ? "" : "hidden"}>
               {barsIcon}
             </span>
@@ -31,49 +32,20 @@ const TraineeNavbar = (props) => {
             </span>
           </i>
 
-          <Navbar.Brand href="#" className={`${navcCass.title}`}> &nbsp; YOTA </Navbar.Brand>
+          <Navbar.Brand href="#" className={`${navcCass.title}`}>
+            {" "}
+            &nbsp; YOTA{" "}
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            {/* <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-
-              <Nav.Link href="#action2">Link</Nav.Link>
-
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-
-                <NavDropdown.Divider />
-
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
-            </Nav> */}
-
-            {/* <Form className="d-flex">
-              <Form.Control
-                type="search"
+            <div className="form-group has-search ">
+              <span className="fa fa-search form-control-feedback"></span>
+              <input
+                type="text"
+                className="form-control"
                 placeholder="Search"
-                className="me-2"
-                aria-label="Search"
               />
-
-              <Button variant="outline-success">Search</Button>
-            </Form> */}
-             
-             
+            </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
