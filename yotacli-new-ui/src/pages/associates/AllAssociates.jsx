b40242f114786f8b9da2356f5d20cb83288bf6c1
mayuri_mundada <mayurimundada13@gmail.com>
2024-04-23 15:46:16 +0530

    Commiting search box code

--- yotacli-new-ui/src/pages/associates/AllAssociates.jsx
+++ yotacli-new-ui/src/pages/associates/AllAssociates.jsx
@@ -1,19 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchAllAssociatesByStatus } from "../../features/associates/associateAction";
 import { TableHeader } from "../../components/table-component/TableHeader";
 import Card from "../../components/Card/Card";
 import Button from 'react-bootstrap/Button';
 import styles from "../../pages/associates/AllAssociates.module.css"
 import { TableBody } from "../../components/table-component/TableBody";
+import { Search } from "../../components/search-component/Search";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 export const AllAssociates = () => {
 
     const { associates } = useSelector((state) => state.associates);
     const { token } = useSelector((state) => state.auth.userData);
     const dispatch = useDispatch();
-    const theadData = ["Select", "User ID", "Name", "Email"];
+    // table 
+    const theadData = ["Select", "Emp ID", "Name", "Email"];
     const tbodyDataKey = ["Select", "userId", "fullName", "emailAdd"];
+    // serach box 
+    const [searchValue, setSearchValue] = useState("");
 
     useEffect(() => {
         if (token)
@@ -30,11 +36,15 @@ export const AllAssociates = () => {
         )
     }
 
-    const tbodyData = associates && associates.map((item) => {
-        const newItem = { ...item };
-        newItem[tbodyDataKey[0]] = getCheckBox();
-        return newItem;
-    });
+    const tbodyData = associates
+        .filter((associateDetails) => Object.values(associateDetails).some(values =>
+            typeof values === 'string' && values.toLowerCase().includes(searchValue.toLowerCase())
+        ))
+        .map((item) => {
+            const newItem = { ...item };
+            newItem[tbodyDataKey[0]] = getCheckBox();
+            return newItem;
+        });
 
     return (
         <>
@@ -44,10 +54,18 @@ export const AllAssociates = () => {
                     associates.length > 0
                         ? (
                             <div>
-                                <Button variant="primary" size="sm" style={{ marginLeft: "83%" }}>
-                                    Assign Selected
-                                </Button>
-                                <table className="table table-bordered table-striped table-hover mt-1">
+                                <Row>
+                                    <Col xs={8}>
+                                        <Search setSearchValue={setSearchValue} />
+                                    </Col>
+                                    <Col>
+                                        <Button variant="primary" size="sm" className={styles["assign-button"]}>
+                                            Assign Selected
+                                        </Button>
+                                    </Col>
+                                </Row>
+
+                                <table className="table table-bordered table-striped table-hover mt-2">
                                     <TableHeader theadData={theadData} />
                                     <TableBody tbodyData={tbodyData} tbodyDataKey={tbodyDataKey} />
                                 </table>
