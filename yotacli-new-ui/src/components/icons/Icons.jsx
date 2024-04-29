29f2cb67d74fd650f48b2ad521c4b1726fd2a89e
yash-raj <yash.raj9501@gmail.com>
2024-04-29 12:06:41 +0530

    add question page added

--- yotacli-new-ui/src/components/icons/Icons.jsx
+++ yotacli-new-ui/src/components/icons/Icons.jsx
@@ -1,5 +1,7 @@
 import {BsPatchCheckFill} from "react-icons/bs";
 import {AiOutlineEdit, AiOutlineStop} from "react-icons/ai";
+import {IoMdAddCircle} from "react-icons/io";
+import {PiUploadSimpleBold} from "react-icons/pi";
 
 export const ApproveIcon = ({
                                 onApprove,
@@ -30,15 +32,43 @@ export const DeclineIcon = ({
 };
 
 export const EditIcon = ({
-    onEdit,
-    title = null,
-    color = null,
-    size = null
-}) => {
-return <AiOutlineEdit
-color={color ? color : "#fd1212"}
-size={size > 10 ? size : "24"}
-cursor={"pointer"}
-title={title ? title : "Edit"}
-onClick={onEdit}/>
+                             onEdit,
+                             title = null,
+                             color = null,
+                             size = null
+                         }) => {
+    return <AiOutlineEdit
+        color={color ? color : "#fd1212"}
+        size={size > 10 ? size : "24"}
+        cursor={"pointer"}
+        title={title ? title : "Edit"}
+        onClick={onEdit}/>
 };
+
+export const AddIcon = ({
+                            onAdd,
+                            title = null,
+                            color = null,
+                            size = null
+                        }) => {
+    return <IoMdAddCircle
+        color={color ? color : "#0d74da"}
+        size={size > 10 ? size : "24"}
+        cursor={"pointer"}
+        title={title ? title : "Add New"}
+        onClick={onAdd}/>
+};
+
+export const UploadIcon = ({
+                               onUpload,
+                               title = null,
+                               color = null,
+                               size = null
+                           }) => {
+    return <PiUploadSimpleBold
+        color={color ? color : "#0d74da"}
+        size={size > 10 ? size : "24"}
+        cursor={"pointer"}
+        title={title ? title : "Upload"}
+        onClick={onUpload}/>
+}
