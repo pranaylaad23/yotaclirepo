f5ec57dfd55323e0b7d0cc420c435b382d7338df
mayuri_mundada <mayurimundada13@gmail.com>
2024-05-15 14:53:41 +0530

    Committing changes

--- yotacli-new-ui/src/constants/helperConstants.js
+++ yotacli-new-ui/src/constants/helperConstants.js
@@ -1,4 +1,4 @@
-export const AXIOS_BASE_URL = "http://localhost:8081/yota-api";
+export const AXIOS_BASE_URL = "http://localhost:8080/yota-api";
 // export const AXIOS_BASE_URL = "http://10.26.2.40:8090/yota-api";
 
 export const TOKEN_KEY = 'auth_token';
