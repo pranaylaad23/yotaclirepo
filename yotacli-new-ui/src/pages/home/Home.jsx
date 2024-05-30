35521e695027051c4926a9f5337db2827c0c19ea
Pankaj Sharma <36689931+code-gram@users.noreply.github.com>
2024-05-30 12:11:48 +0530

    issue resolved for StartTest, Home and Approute

--- yotacli-new-ui/src/pages/home/Home.jsx
+++ yotacli-new-ui/src/pages/home/Home.jsx
@@ -4,7 +4,6 @@ import { logout } from "../../features/login/loginAction";
 import TestPaper from "../associates/student/TestPaper";
 import MyTestButton from "../associates/MyTestButton";
 import { USER_ROLES } from "../../constants/helperConstants";
-import StartTest from "../associates/student/StartTest";
 import StudentDashboard from "../associates/student/StudentDashboard";
 
 export const Home = () => {
@@ -33,8 +32,7 @@ export const Home = () => {
           <h3>Welcome, {userData.fullName}</h3>
           <br />
           <div>
-            {/* <StartTest /> */}
-            <StudentDashboard/>
+            <StudentDashboard />
           </div>
         </div>
       )}
--- yotacli-new-ui/src/routes/AppRoutes.jsx
+++ yotacli-new-ui/src/routes/AppRoutes.jsx
@@ -38,7 +38,7 @@ import { TPR } from "../pages/training-performance-report/TPR";
 import { AddTest } from "../pages/test/AddTest";
 import { AssociatesList } from "../pages/associates/AssociatesList";
 import { ProfileDetails } from "../pages/profile-details/ProfileDetails";
-import StartTest from "../../src/pages/associates/student/StartTest";
+import StartTest from "../../src/pages/associates/StartTest";
 export const AppRoutes = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -151,7 +151,7 @@ export const AppRoutes = () => {
           <Route path={"/myTrainings"} element={<MyTrainings />} />
           <Route path={"/myTest"} element={<MyTest />} />
           <Route path={"/profile-details"} element={<ProfileDetails />} />
-          <Route path={"/starttest"} element={<StartTest/>} />
+          <Route path={"/starttest"} element={<StartTest />} />
           <Route path={"/reports"} element={"/"} />
         </>
       )}
