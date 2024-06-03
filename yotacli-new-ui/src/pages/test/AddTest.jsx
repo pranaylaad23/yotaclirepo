f93afbd54e48d17641cbf8581830788451292a1b
Pankaj Sharma <36689931+code-gram@users.noreply.github.com>
2024-06-03 11:29:09 +0530

    AddTest Options are on now

--- yotacli-new-ui/src/pages/test/AddTest.jsx
+++ yotacli-new-ui/src/pages/test/AddTest.jsx
@@ -4,26 +4,20 @@ import { TestSetting } from "./TestSetting";
 import { AddQuestion } from "./AddQuestion";
 
 export const AddTest = () => {
+  const [nextScreen, setNextScreen] = useState("screen1");
 
-    const [nextScreen, setNextScreen] = useState("screen1");
-
-    const renderScreen = () => {
-        switch (nextScreen) {
-            // case 'screen1':
-            //     return <BasicInfo nextScreen={setNextScreen} />;
-            // case 'screen2':
-            //     return <TestSetting nextScreen={setNextScreen} />;
-            // case 'screen3':
-            //     return <AddQuestion />
-            default:
-                return <AddQuestion />
-        }
+  const renderScreen = () => {
+    switch (nextScreen) {
+      case "screen1":
+        return <BasicInfo nextScreen={setNextScreen} />;
+      case "screen2":
+        return <TestSetting nextScreen={setNextScreen} />;
+      case "screen3":
+        return <AddQuestion />;
+      default:
+        return <AddQuestion />;
     }
+  };
 
-
-    return (
-        <div>
-           {renderScreen()}
-        </div>
-    )
-}
+  return <div>{renderScreen()}</div>;
+};
